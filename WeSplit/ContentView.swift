7970d2e587d08a4a45855caf37bb08003fa5cae4
Natalia Moreno <natlcs20@icloud.com>
2020-11-04 10:55:57 -0500

    Added navigation

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,17 +8,18 @@
 import SwiftUI
 
 struct ContentView: View {
-    let students = ["Jose", "Juan", "Marselo"]
-    @State private var selectedStudent = "Jose"
-    
-    var body: some View {
-        Picker("select your student", selection: $selectedStudent) {
-            ForEach(0 ..< students.count) {
-                Text(self.students[$0])
+    var body: some View  {
+        NavigationView {
+            Form {
+                Section {
+                    Text ("Hello world")
+                }
+            }
+            .navigationBarTitle(Text("SwiftUI"), displayMode:
+                 .inline)
             }
         }
     }
-}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
