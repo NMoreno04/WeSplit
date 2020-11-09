11805bc270936de1a29dc667a587d399fc3b905c
Natalia Moreno <natlcs20@icloud.com>
2020-11-09 11:48:08 -0500

    Added text field

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,16 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
-@State private var name = ""
+    @State private var checkAmount = ""
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 2
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
     var body: some View {
-        Form {
-            TextField ("Enter your name", text: $name)
-            Text("Your name is \(name)")
+            Form {
+                Section {
+                    TextField("Amount", text: $checkAmount)
+                }
+                Section {
+                    Text("$\(checkAmount)")
+                }
             }
-         }
+        }
     }
-    
-    
     
     
 struct ContentView_Previews: PreviewProvider {
